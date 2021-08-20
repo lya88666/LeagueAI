5e1a43388c8f0655a89fdf6b51f5320132c0ab26
nicole2020hsustain <61095596+nicole2020hsustain@users.noreply.github.com>
2021-08-20 23:26:08 +0200

    Empty project

---
+++ LeagueAI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LeagueAI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
